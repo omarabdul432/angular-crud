860f43015fc86c97025bbc62739f906108df0055
omarabdul432 <abdomar0748@gmail.com>
2024-08-24 13:25:09 +0530

    updated

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyAW15bUuniycl4n56JOPlNIVH7funt3LLU",
+        authDomain: "angular-firebase-crud-c54f7.firebaseapp.com",
+        projectId: "angular-firebase-crud-c54f7",
+        storageBucket: "angular-firebase-crud-c54f7.appspot.com",
+        messagingSenderId: "800189337557",
+        appId: "1:800189337557:web:cd4af919d4c6dcac4bb75c"
+      }
+}
