9cb6150a919e2186c8c515356635279fda4d1600
omarabdul432 <abdomar0748@gmail.com>
2024-08-24 13:01:30 +0530

    updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,20 @@
-export const environment = {};
+import { initializeApp } from "firebase/app";
+export const environment = {
+    // Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+production:false,
+ firebaseConfig : {
+  apiKey: "AIzaSyAW15bUuniycl4n56JOPlNIVH7funt3LLU",
+  authDomain: "angular-firebase-crud-c54f7.firebaseapp.com",
+  projectId: "angular-firebase-crud-c54f7",
+  storageBucket: "angular-firebase-crud-c54f7.appspot.com",
+  messagingSenderId: "800189337557",
+  appId: "1:800189337557:web:cd4af919d4c6dcac4bb75c"
+}
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+export const environment = {
+    production:false,
+    // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+ firebaseConfig : {
   apiKey: "AIzaSyAW15bUuniycl4n56JOPlNIVH7funt3LLU",
   authDomain: "angular-firebase-crud-c54f7.firebaseapp.com",
   projectId: "angular-firebase-crud-c54f7",
   storageBucket: "angular-firebase-crud-c54f7.appspot.com",
   messagingSenderId: "800189337557",
   appId: "1:800189337557:web:cd4af919d4c6dcac4bb75c"
-};
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+}
+// const app = initializeApp(firebaseConfig);
